6ca55472e564ff22e94e84facb8a222ff5d48b59
Lahari Gandla(UST <304579@ust.com>
2026-06-27 11:54:28 +0530

    CI pipeline step 6

--- src/test/java/com/ust/sdet/pact/PosOmsConsumerPactTest.java
+++ src/test/java/com/ust/sdet/pact/PosOmsConsumerPactTest.java
@@ -1,151 +1,112 @@
 package com.ust.sdet.pact;
 
+
+import java.util.Map;
+
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
-
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(PactConsumerTestExt.class)
-@PactTestFor(providerName = "oms-provider")
-public class PosOmsConsumerPactTest {
 
-    @Pact(consumer = "pos-consumer")
-    public V4Pact getOrder(PactDslWithProvider builder) {
+@PactTestFor(providerName = "oms-provider",pactVersion = PactSpecVersion.V4)
 
+class PosOmsConsumerPactTest {
+
+    @Pact(provider = "oms-provider", consumer = "pos-consumer")
+    V4Pact getOrder(PactDslWithProvider builder) {
         return builder
-                .given("Order exists")
-                .uponReceiving("Get order details")
-                .path("/orders/1001")
+                .given("Order 245 exists")
+                .uponReceiving("a request for order 123")
+                .path("/order/123")
                 .method("GET")
                 .willRespondWith()
                 .status(200)
+                .matchHeader("Content-Type", "application/json(;.*)?", "application/json")
                 .body(new PactDslJsonBody()
-                        .integerType("orderId", 1001)
-                        .stringType("status", "CONFIRMED"))
-                .toPact(V4Pact.class);
-    }
-
-    @Pact(consumer = "pos-consumer")
-    public V4Pact createOrder(PactDslWithProvider builder) {
-
-        return builder
-                .given("inventory available for SKU-9")
-                .uponReceiving("a request to create an order")
-                .path("/orders")
-                .method("POST")
-                .headers(Map.of("Content-Type", "application/json"))
-                .body(new PactDslJsonBody()
-                        .stringType("sku", "SKU-9")
-                        .integerType("qty", 1))
-                .willRespondWith()
-                .status(201)
-                .matchHeader("Location",
-                        "/orders/\\d+",
-                        "/orders/777")
-                .body(new PactDslJsonBody()
-                        .integerType("id", 777)
-                        .stringType("status", "PENDING"))
-                .toPact(V4Pact.class);
-    }
-
-    @Pact(consumer = "pos-consumer")
-    public V4Pact getInventory(PactDslWithProvider builder) {
-
-        return builder
-                .given("SKU-9 has stock")
-                .uponReceiving("a request for SKU-9 inventory")
-                .path("/inventory/SKU-9")
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(new PactDslJsonBody()
-                        .stringType("sku", "SKU-9")
-                        .integerType("qty", 5))
+                        .integerType("orderId", 123)
+                        .stringType("status", "CONFIRMED")
+                        .numberType("total", 42.0))
                 .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "getOrder")
-    void getOrderTest(MockServer mockServer) {
+    void testGetOrder(MockServer mockServer) {
 
-        Response response =
-                given()
-                        .baseUri(mockServer.getUrl())
-                        .when()
-                        .get("/orders/1001")
-                        .then()
-                        .extract()
-                        .response();
+        var order = new OmsClient(mockServer.getUrl()).getOrder(123);
+        assertEquals(123, order.orderId());
+        assertEquals("CONFIRMED", order.status());
+        assertEquals(42.0, order.total(),0);
 
-        assertEquals(200, response.statusCode());
-        assertEquals(1001,
-                response.jsonPath().getInt("orderId"));
-        assertEquals("CONFIRMED",
-                response.jsonPath().getString("status"));
+    }
+
+    @Pact(provider = "oms-provider", consumer = "pos-consumer")
+    V4Pact createOrder(PactDslWithProvider builder){
+        return builder
+                .given("Creating a new order")
+                .uponReceiving("Upon New order Creating")
+                .body( new PactDslJsonBody()
+                        .integerType("statusCode",0)
+                        .integerType("orderId",0)
+                        .stringType("status","NEW")
+                        .decimalType("total",2000.0)
+                )
+                .path("/orders/")
+                .headers(Map.of("Content-Type", "application/json"))
+                .method("POST")
+                .willRespondWith()
+                .status(201)
+                .matchHeader("Content-Type", "application/json(;.*)?", "application/json")
+                .body(
+                        new PactDslJsonBody()
+                                .integerType("statusCode",201)
+                                .integerType("orderId",101)
+                                .stringType("status","CREATED")
+                                .integerType("total",2000)
+                ).toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "createOrder")
-    void createOrderTest(MockServer mockServer) {
+    void testcreateOrder(MockServer mockServer){
+        var newOrder = new OmsClient.Order(201,0,"NEW",2000);
+        var createdorder = new OmsClient(mockServer.getUrl()).createOrder(newOrder);
+        assertEquals(createdorder.orderId(), 101);
+        assertEquals(createdorder.status(), "CREATED");
+        assertEquals(newOrder.total(), createdorder.total(),0);
+    }
 
-        String requestBody = """
-                {
-                  "sku":"SKU-9",
-                  "qty":1
-                }
-                """;
-
-        Response response =
-                given()
-                        .baseUri(mockServer.getUrl())
-                        .contentType("application/json")
-                        .body(requestBody)
-                        .when()
-                        .post("/orders")
-                        .then()
-                        .extract()
-                        .response();
-
-        assertEquals(201, response.statusCode());
-
-        assertNotNull(response.header("Location"));
-
-        assertEquals("PENDING",
-                response.jsonPath().getString("status"));
+    @Pact(provider = "oms-provider", consumer = "pos-consumer")
+    V4Pact getInventory(PactDslWithProvider builder){
+        return builder
+                .given("SKU-9 has Stock")
+                .uponReceiving("a request for SKU-9 inventory")
+                .path("/inventory/SKU-9")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(
+                        new PactDslJsonBody()
+                                .stringType("sku","SKU-9")
+                                .integerType("qty",5)
+                ).toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "getInventory")
-    void getInventoryTest(MockServer mockServer) {
-
-        Response response =
-                given()
-                        .baseUri(mockServer.getUrl())
-                        .when()
-                        .get("/inventory/SKU-9")
-                        .then()
-                        .extract()
-                        .response();
-
-        assertEquals(200, response.statusCode());
-
-        assertEquals("SKU-9",
-                response.jsonPath().getString("sku"));
-
-        assertTrue(
-                response.jsonPath().getInt("qty") >= 0
-        );
+    void testgetInventory(MockServer mockServer){
+        var inventoryResponse = new OmsClient(mockServer.getUrl()).getInventory();
+        assertEquals(inventoryResponse.path("sku"), "SKU-9");
+        assertEquals((int)inventoryResponse.path("qty"), 5);
     }
 }
